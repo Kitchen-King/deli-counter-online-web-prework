4c72a7944c72660b836216197aa2c90166f6c5b8
Kyle Spencer <spencerk2@vcu.edu>
2019-01-26 21:48:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,13 @@ def now_serving(array)
   end
   array.delete_at(0)
 end
+
+def line(array)
+if array.length != 0 then
+puts "The line is currently: "
+array.each do |name|
+   puts " #{array.index(name) + 1}. #{name}"
+end
+else puts"There is nobody waiting to be served!"
+end
+end
