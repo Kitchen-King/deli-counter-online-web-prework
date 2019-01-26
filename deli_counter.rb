ba82ef208dd9ae9f1ac72accff1e821a74debbb2
Kyle Spencer <spencerk2@vcu.edu>
2019-01-26 21:38:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+end
+
+def now_serving(array)
+  if array.length == 0 then
+  puts "There is nobody waiting to be served!"
+  elsif puts "Currently serving #{array.first}."
+  end
+  array.delete_at(0)
+end
